588ea4e76d233ce6557cc3dfd9d264eb78116cd8
ThunderYY <916717719@qq.com>
2017-01-16 14:33:43 +0800

    [fix]table.js

--- superset/assets/visualizations/table.js
+++ superset/assets/visualizations/table.js
@@ -36,50 +36,50 @@ function tableVis(slice) {
     }
 
     // add a modal
-    function showModal(title, url) {
-      let modals;
-      if ($('#modals').attr('id') !== undefined) {
-        modals = $('#modals');
-      } else {
-        modals = document.createElement('div');
-        $(modals).attr('id', 'modals');
-        document.body.append(modals);
-      }
-      const myModal = document.createElement('div');
-      const modalCount = $('#modals').children().length;
-      $(myModal).attr('id', modalCount)
-      .attr('class', 'modal fade')
-      .attr('role', 'dialog')
-      .attr('aria-hidden', true)
-      .attr('id', 'newSlice_' + modalCount);
-      const modalDialog = document.createElement('div');
-      $(modalDialog).attr('class', 'modal-dialog');
-      const modalContent = document.createElement('div');
-      $(modalContent).attr('class', 'modal-content');
-      const modalHeader = document.createElement('div');
-      $(modalHeader).attr('class', 'modal-header');
-      const modalTitle = document.createElement('h4');
-      $(modalTitle).attr('class', 'modal-title')
-      .text(title);
-      const modalBody = document.createElement('div');
-      $(modalBody).attr('class', 'modal-body');
-      const iframe = document.createElement('iframe');
-      $(iframe).attr('id', 'iframe_' + modalCount)
-      .attr('src', url)
-      .attr('height', '50%')
-      .attr('width', '100%')
-      .attr('frameborder', 0);
-      $(modalBody).append(iframe);
-      $(modalContent).append(modalHeader);
-      $(modalContent).append(modalBody);
-      $(modalHeader).append(modalTitle);
-      $(modalDialog).append(modalContent);
-      $(myModal).append(modalDialog);
-      $(modals).append(myModal);
-      $(myModal).draggable({
-        handle: '.modal-header',
-      });
-      $(myModal).modal({ show: true });
+    // function showModal(title, url) {
+    //   let modals;
+    //   if ($('#modals').attr('id') !== undefined) {
+    //     modals = $('#modals');
+    //   } else {
+    //     modals = document.createElement('div');
+    //     $(modals).attr('id', 'modals');
+    //     document.body.append(modals);
+    //   }
+    //   const myModal = document.createElement('div');
+    //   const modalCount = $('#modals').children().length;
+    //   $(myModal).attr('id', modalCount)
+    //   .attr('class', 'modal fade')
+    //   .attr('role', 'dialog')
+    //   .attr('aria-hidden', true)
+    //   .attr('id', 'newSlice_' + modalCount);
+    //   const modalDialog = document.createElement('div');
+    //   $(modalDialog).attr('class', 'modal-dialog');
+    //   const modalContent = document.createElement('div');
+    //   $(modalContent).attr('class', 'modal-content');
+    //   const modalHeader = document.createElement('div');
+    //   $(modalHeader).attr('class', 'modal-header');
+    //   const modalTitle = document.createElement('h4');
+    //   $(modalTitle).attr('class', 'modal-title')
+    //   .text(title);
+    //   const modalBody = document.createElement('div');
+    //   $(modalBody).attr('class', 'modal-body');
+    //   const iframe = document.createElement('iframe');
+    //   $(iframe).attr('id', 'iframe_' + modalCount)
+    //   .attr('src', url)
+    //   .attr('height', '50%')
+    //   .attr('width', '100%')
+    //   .attr('frameborder', 0);
+    //   $(modalBody).append(iframe);
+    //   $(modalContent).append(modalHeader);
+    //   $(modalContent).append(modalBody);
+    //   $(modalHeader).append(modalTitle);
+    //   $(modalDialog).append(modalContent);
+    //   $(myModal).append(modalDialog);
+    //   $(modals).append(myModal);
+    //   $(myModal).draggable({
+    //     handle: '.modal-header',
+    //   });
+    //   $(myModal).modal({ show: true });
       // const myModal = $('#newSlice').clone();
       // const modalCount = $('#modals').children().length;
       // myModal.attr('id', 'newSlice_' + modalCount);
@@ -95,80 +95,79 @@ function tableVis(slice) {
       // $('.modal-backdrop').each(function () {
       //   $(this).attr('id', 'id_' + Math.random());
       // });
-    }
+    // }
 
     let left = 10;
     const isIE = (document.all) ? true : false;
-    const Extend = function(destination, source) {
+    const Extend = function (destination, source) {
       for (const property in source) {
         destination[property] = source[property];
       }
     };
-    const Bind = function(object, fun, args) {
-      return function() {
+    const Bind = function (object, fun, args) {
+      return function () {
         return fun.apply(object, args || []);
-      }
+      };
     };
-    const BindAsEventListener = function(object, fun) {
-       const args = Array.prototype.slice.call(arguments).slice(2);
-       return function(event) {
-         return fun.apply(object, [event || window.event].concat(args));
-       }
+    const BindAsEventListener = function (object, fun) {
+      const args = Array.prototype.slice.call(arguments).slice(2);
+      return function (event) {
+        return fun.apply(object, [event || window.event].concat(args));
+      };
     };
-    const CurrentStyle = function(element) {
-       return element.currentStyle || document.defaultView.getComputedStyle(element, null);
+    const CurrentStyle = function (element) {
+      return element.currentStyle || document.defaultView.getComputedStyle(element, null);
     };
     function create(elm, parent, fn) {
       const element = document.createElement(elm);
-      fn&&fn(element);
-      parent&&parent.appendChild(element);
+      fn && fn(element);parent && parent.appendChild(element);
       return element;
-    };
+    }
     function addListener(element, e, fn) {
       element.addEventListener ?
       element.addEventListener(e, fn, false) :
-      element.attachEvent("on" + e, fn);
-    };
+      element.attachEvent('on' + e, fn);
+    }
     function removeListener(element, e, fn) {
       element.removeEventListener ?
-      element.removeEventListener(e,fn , false) :
-      element.detachEvent("on" + e,fn);
-    };
-    const Class = function(properties) {
-      let _class = function() {
-        return (arguments[0] !== null && this.initialize && typeof(this.initialize) == 'function') ?
-        this.initialize.apply(this, arguments) : this;
+      element.removeEventListener(e, fn, false) :
+      element.detachEvent('on' + e, fn);
+    }
+    const Class = function (properties) {
+      const eleClass = function () {
+        return (arguments[0] !== null && this.init && typeof(this.init) == 'function') ?
+        this.init.apply(this, arguments) : this;
       };
-      _class.prototype = properties;
-      return _class;
+      eleClass.prototype = properties;
+      return eleClass;
     };
 
-    let Dialog = new Class({
-      options:{
-        Width : 300, 
-        Height : 300,
-        Left : 100,
-        Top : 100,
-        Titleheight : 26,
-        Minwidth : 200,
-        Minheight : 200,
-        CancelIco : true,
-        ResizeIco : false,
-        Info : "",
-        Content : "",
-        Zindex : 10,
-      }, 
-      initialize : function(options) {
-        this._dragobj = null;
-        this._resize = null;
-        this._cancel = null;
-        this._body = null;
-        this._x = 0;
-        this._y = 0;
-        this._fM = BindAsEventListener(this, this.Move);
-        this._fS = Bind(this, this.Stop);
-        this._isdrag = null;
-        this._Css = null;
+    const Dialog = new Class({
+      options: {
+        Width: 300,
+        Height: 300,
+        Left: 100,
+        Top: 100,
+        Titleheight: 26,
+        Minwidth: 200,
+        Minheight: 200,
+        CancelIco: true,
+        ResizeIco: false,
+        Info: '',
+        Content: '',
+        Zindex: 10,
+      },
+      init: function (options) {
+        this.optDragobj = null;
+        this.optResize = null;
+        this.optCancel = null;
+        this.optBody = null;
+        this.optX = 0;
+        this.optY = 0;
+        this.optFM = BindAsEventListener(this, this.Move);
+        this.optFS = Bind(this, this.Stop);
+        this.optIsdrag = null;
+        this.optCss = null;
         this.Width = this.options.Width;
         this.Height = this.options.Height;
         this.Left = this.options.Left;
@@ -178,16 +177,18 @@ function tableVis(slice) {
         this.Content = this.options.Content;
         this.Minwidth = this.options.Minwidth;
         this.Minheight = this.options.Minheight;
-        this.Titleheight= this.options.Titleheight;
+        this.Titleheight = this.options.Titleheight;
         this.Zindex = this.options.Zindex;
-        Extend(this,options);
+        Extend(this, options);
         Dialog.Zindex = this.Zindex;
   // 构造dialog
-        let obj = ['dialogcontainter', 'dialogtitle', 'dialogtitleinfo', 'dialogtitleico',
+        const obj = ['dialogcontainter', 'dialogtitle', 'dialogtitleinfo', 'dialogtitleico',
         'dialogbody', 'dialogbottom'];
-        for(let i = 0;i < obj.length; i++) {
-          obj[i] = create('div', null, function(elm) {
+        for (let i = 0; i < obj.length; i++) {
+          obj[i] = create('div', null, function(e) {
+            let elm = e;
             elm.className = obj[i];
+            return elm;
           });
         }
         obj[2].innerHTML = this.Info;
@@ -198,92 +199,91 @@ function tableVis(slice) {
         obj[0].appendChild(obj[4]);
         obj[0].appendChild(obj[5]);
         document.body.appendChild(obj[0]);
-        this._dragobj = obj[0];
-        this._resize = obj[5];
-        this._cancel = obj[3];
-        this._body = obj[4];
-    ////////////////////////////////////////////////////////////////////////////////o,x1,x2 
-    ////设置Dialog的长 宽 ,left ,top 
+        this.optDragobj = obj[0];
+        this.optResize = obj[5];
+        this.optCancel = obj[3];
+        this.optBody = obj[4];
+    // o,x1,x2
+    // 设置Dialog的长 宽 ,left ,Top
     // with(this._dragobj.style){
-    //      height = this.Height + "px";top = this.Top + "px";width = this.Width +"px";left = this.Left + "px";zIndex = this.Zindex; 
-    // } 
-        this._dragobj.style.height = this.Height + 'px';
-        this._dragobj.style.top = this.Top + 'px';
-        this._dragobj.style.width = this.Width + 'px';
-        this._dragobj.style.left = this.Left + 'px';
-        this._dragobj.style.zIndex = this.Zindex;
-        this._body.style.height =
-        this.Height - this.Titleheight-parseInt(CurrentStyle(this._body).paddingLeft)*2+'px'; 
-        /////////////////////////////////////////////////////////////////////////////// 添加事件 
-        addListener(this._dragobj, 'mousedown', BindAsEventListener(this, this.Start, true));
-        addListener(this._cancel, 'mouseover', Bind(this,this.Changebg,
-        [this._cancel, '0px 0px', '-21px 0px']));
-        addListener(this._cancel, 'mouseout', Bind(this, this.Changebg,
-        [this._cancel, '0px 0px', '-21px 0px']));
-        addListener(this._cancel, 'mousedown' ,BindAsEventListener(this, this.Disappear));
-        addListener(this._body, 'mousedown', BindAsEventListener(this, this.Cancelbubble));
-        addListener(this._resize, 'mousedown', BindAsEventListener(this, this.Start, false));
-        },
-        Disappear : function(e) {
+    //      height = this.Height + "px";top = this.Top + "px";width = this.Width +"px";
+    //      left = this.Left + "px";zIndex = this.Zindex;
+    // }
+        this.optDragobj.style.height = this.Height + 'px';
+        this.optDragobj.style.top = this.Top + 'px';
+        this.optDragobj.style.width = this.Width + 'px';
+        this.optDragobj.style.left = this.Left + 'px';
+        this.optDragobj.style.zIndex = this.Zindex;
+        this.optBody.style.height =
+        this.Height - this.Titleheight-parseInt(CurrentStyle(this.optBody).paddingLeft) * 2 + 'px'; 
+        /// 添加事件
+        addListener(this.optDragobj, 'mousedown', BindAsEventListener(this, this.Start, true));
+        addListener(this.optCancel, 'mouseover', Bind(this, this.Changebg,
+        [this.optCancel, '0px 0px', '-21px 0px']));
+        addListener(this.optCancel, 'mouseout', Bind(this, this.Changebg,
+        [this.optCancel, '0px 0px', '-21px 0px']));
+        addListener(this.optCancel, 'mousedown', BindAsEventListener(this, this.Disappear));
+        addListener(this.optBody, 'mousedown', BindAsEventListener(this, this.Cancelbubble));
+        addListener(this.optResize, 'mousedown', BindAsEventListener(this, this.Start, false));
+      },
+      Disappear: function (e) {
+        this.Cancelbubble(e);
+        document.body.removeChild(this.optDragobj);
+      },
+      Cancelbubble: function (e) {
+        this.optDragobj.style.zIndex = ++Dialog.Zindex;
+        document.all ? (e.cancelBubble = true) : (e.stopPropagation());
+      },
+      Changebg: function (o, x1, x2) {
+        o.style.backgroundPosition = (o.style.backgroundPosition === x1) ? x2 : x1;
+      },
+      Start: function (e, isdrag) {
+        if (!isdrag) {
           this.Cancelbubble(e);
-          document.body.removeChild(this._dragobj);
-        },
-        Cancelbubble : function(e) {
-          this._dragobj.style.zIndex = ++Dialog.Zindex;
-          document.all ? (e.cancelBubble = true) : (e.stopPropagation());
-        },
-        Changebg : function(o, x1, x2) {
-          o.style.backgroundPosition = (o.style.backgroundPosition == x1) ? x2 : x1;
-        },
-        Start : function(e, isdrag) {
-          if (!isdrag) {
-            this.Cancelbubble(e);
-          }
-          this._Css = isdrag ? { x : "left", y : "top" } : { x : "width", y : "height" };
-          this._dragobj.style.zIndex = ++Dialog.Zindex;
-          this._isdrag = isdrag;
-          this._x = isdrag ? (e.clientX - this._dragobj.offsetLeft || 0) :
-          (this._dragobj.offsetLeft || 0);
-          this._y = isdrag ? (e.clientY - this._dragobj.offsetTop || 0) :
-          (this._dragobj.offsetTop || 0);
-          if (isIE) {
-            addListener(this._dragobj, "losecapture", this._fS);
-            this._dragobj.setCapture();
-          } else {
-            e.preventDefault();
-            addListener(window, "blur", this._fS);
-          }
-          addListener(document, 'mousemove', this._fM);
-          addListener(document, 'mouseup', this._fS);
-        },
-        Move : function(e) {
-          window.getSelection ? window.getSelection().removeAllRanges() :
-          document.selection.empty();
-
-          //here
-          const i_x = e.clientX - this._x, i_y = e.clientY - this._y;
-          this._dragobj.style[this._Css.x] = (this._isdrag ? Math.max(i_x, 0) :
-          Math.max(i_x, this.Minwidth)) + 'px';
-          this._dragobj.style[this._Css.y] = (this._isdrag ? Math.max(i_y, 0) :
-          Math.max(i_y, this.Minheight)) + 'px';
-          if (!this._isdrag) {
-            this._body.style.height = Math.max(i_y - this.Titleheight, 
-            this.Minheight - this.Titleheight) -
-            2 * parseInt(CurrentStyle(this._body).paddingLeft) + 'px';
-          }
-        },
-        Stop : function() {
-          removeListener(document, 'mousemove', this._fM);
-          removeListener(document, 'mouseup', this._fS);
-          if (isIE) {
-            removeListener(this._dragobj, "losecapture", this._fS); 
-            this._dragobj.releaseCapture();
-          } else {
-            removeListener(window, "blur", this._fS);
-          };
         }
+        this.optCss = isdrag ? { x: 'left', y: 'top' } : { x: 'width', y: 'height' };
+        this.optDragobj.style.zIndex = ++Dialog.Zindex;
+        this.optIsdrag = isdrag;
+        this.optX = isdrag ? (e.clientX - this.optDragobj.offsetLeft || 0) :
+        (this.optDragobj.offsetLeft || 0);
+        this.optY = isdrag ? (e.clientY - this.optDragobj.offsetTop || 0) :
+        (this.optDragobj.offsetTop || 0);
+        if (isIE) {
+          addListener(this.optDragobj, 'losecapture', this.optFS);
+          this.optDragobj.setCapture();
+        } else {
+          e.preventDefault();
+          addListener(window, "blur", this.optFS);
+        }
+        addListener(document, 'mousemove', this.optFM);
+        addListener(document, 'mouseup', this.optFS);
+      },
+      Move: function (e) {
+        window.getSelection ? window.getSelection().removeAllRanges() :
+        document.selection.empty();
+        const iX = e.clientX - this.optX, iY = e.clientY - this.optY;
+        this.optDragobj.style[this.optCss.x] = (this.optIsdrag ? Math.max(iX, 0) :
+        Math.max(iX, this.Minwidth)) + 'px';
+        this.optDragobj.style[this.optCss.y] = (this.optIsdrag ? Math.max(iY, 0) :
+        Math.max(iY, this.Minheight)) + 'px';
+        if (!this.optIsdrag) {
+          this.optBody.style.height = Math.max(iY - this.Titleheight, 
+          this.Minheight - this.Titleheight) -
+          2 * parseInt(CurrentStyle(this.optBody).paddingLeft) + 'px';
+        }
+      },
+        Stop: function () {
+          removeListener(document, 'mousemove', this.optFM);
+          removeListener(document, 'mouseup', this.optFS);
+          if (isIE) {
+            removeListener(this.optDragobj, 'losecapture', this.optFS);
+            this.optDragobj.releaseCapture();
+          } else {
+            removeListener(window, 'blur', this.optFS);
+          }
+        },
     });
-function creat(title, url){
+    function creat(title, url) {
       let modals;
       if ($('#modals').attr('id') !== undefined) {
         modals = $('#modals');
@@ -292,14 +292,12 @@ function creat(title, url){
         $(modals).attr('id', 'modals');
         document.body.append(modals);
       }
-      const myModal = document.createElement('div');
       const modalCount = $('#modals').children().length;
-      let content = '<iframe id = "newSlice_' + modalCount + '" width = "100%" height = "100%" src = "' + url + '"> </iframe>';
-      new Dialog({ Info: title, Left:300 + left, Content: content, Zindex: (++Dialog.Zindex) });
+      const content = '<iframe id = "newSlice_' + modalCount +
+      '" width = "100%" height = "100%" src = "' + url + '"> </iframe>';
+      new Dialog({ Info: title, Left: 300 + left, Content: content, Zindex: (++Dialog.Zindex) });
       left += 10;
-} 
-
-
+    }
 
     // add listener to get navigate message
     $(document).ready(function () {
