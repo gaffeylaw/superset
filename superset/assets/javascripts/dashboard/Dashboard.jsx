3fb44d2dad7c6609c20d8beceb52dc30e6390f5f
ThunderYY <916717719@qq.com>
2017-02-27 14:53:18 +0800

    [add]navigate mutiple targets

--- superset/assets/javascripts/dashboard/Dashboard.jsx
+++ superset/assets/javascripts/dashboard/Dashboard.jsx
@@ -14,6 +14,7 @@ import Header from './components/Header';
 require('bootstrap');
 require('../../stylesheets/dashboard.css');
 require('../superset-select2.js');
+require('../../visualizations/table.css');
 
 export function getInitialState(dashboardData, context) {
   const dashboard = Object.assign({ context }, utils.controllerInterface, dashboardData);
